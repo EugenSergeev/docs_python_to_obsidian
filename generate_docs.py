5cd40812af65ffd2b63311cfd6497d7abaf00dbc
Evgeny Sergeev <flashser@yandex.ru>
2023-01-17 00:24:27 +0600

    refactor

--- generate_docs.py
+++ generate_docs.py
@@ -5,7 +5,7 @@ from loguru import logger
 START_ROWS = "Здесь будут комментарии из файла\n\n\n\n\n\n\n\n\n\n---\n"
 
 CURRENT_DIR = Path().cwd()
-ENV_PATH_NAME = "venv"
+VENV_PATH_NAME = "venv"
 DOCS_PATH_NAME = "Docs"
 
 
@@ -22,13 +22,16 @@ class Import:
 
 
 class PyFile:
-    def __init__(self, file_name: str | Path, project_path: str| Path):
+    def __init__(self, file_name: str | Path, project_path: str | Path):
         self.file = Path(file_name)
         self.project_path = Path(project_path)
         self.file_rows = self.get_rows_of_py_file()
         self.classes = self.get_classes()
-        self.import_objects = self.get_imports()
+        self.import_elements = self.get_imports()
         self.filename = self.file.as_posix()
+
+    def get_uri(self):
+        return (self.project_path / self.file).absolute().as_uri()
 
     def get_rows_of_py_file(self):
         file = self.project_path / self.file
@@ -89,6 +92,9 @@ class PyFile:
     def __str__(self):
         return self.__repr__()
 
+    def __hash__(self):
+        return hash(self.filename)
+
 
 def get_all_py_files_without_env(env_path, path_project):
     all_py_files = [file for file in path_project.glob("**/*.py")
@@ -101,75 +107,70 @@ def get_all_py_files_without_env(env_path, path_project):
         py_files.append(file_)
     return py_files
 
+
 @logger.catch()
-def start_generate(project_path=CURRENT_DIR, env_path_name=ENV_PATH_NAME, docs_path=None):
+def start_generate(project_path=CURRENT_DIR, env_path_name=VENV_PATH_NAME, docs_path=None):
     project_path = Path(project_path)
     docs_path = Path(docs_path) if docs_path else Path().cwd() / DOCS_PATH_NAME / project_path.name
     env_path = Path(project_path) / Path(env_path_name)
+
     py_files = get_all_py_files_without_env(env_path, project_path)
     py_file_objects = [PyFile(file_path, project_path) for file_path in py_files]
 
-    files_text_dict = {}  # тут хранятся строки для всех файлов MD
-    for py_file in py_files:
-        rows = [START_ROWS, f'[Link to file]({py_file.absolute().as_uri()})']
-        files_text_dict[py_file.as_posix()] = rows
-
-    all_classes_links = {py_obj.get_module_name_for_import() + "." + class_: f"{py_obj.filename}#{class_}"
+    all_classes_links = {py_obj.get_module_name_for_import() + "." + class_
                          for py_obj in py_file_objects
                          for class_ in py_obj.classes}
 
+    # тут хранятся строки для всех файлов MD
+    files_text_dict = {py_obj: {"Header": START_ROWS,
+                                "link_to_file": f'[Link to file]({(py_obj.get_uri())})',
+                                "# Implemented classes": [f'#### {class_}' for class_ in py_obj.classes],
+                                "# Imported modules": [],
+                                "# Imported classes": [],
+                                "# Other imports": []
+                                }
+                       for py_obj in py_file_objects}
+
     py_file_import_classes_dict = {}
     for py_obj in py_file_objects:
-        if py_obj.classes:
-            files_text_dict[py_obj.filename].append(f'# Implemented classes')
-            for class_ in py_obj.classes:
-                files_text_dict[py_obj.filename].append(f'#### {class_}')
-
-        modules = []
-        for import_ in py_obj.import_objects:
+        for import_ in py_obj.import_elements:
             module_name_path = Path(import_.abs_path.replace(".", "/"))
-
             for i in range(len(module_name_path.parts), 0, -1):
                 name = "/".join(module_name_path.parts[:i])
                 name_with_py = Path(str(name) + ".py")
                 if name_with_py in py_files:
                     row = f"[[{name_with_py.as_posix()}|{name_with_py.name}]]"
-                    modules.append(row)
+                    files_text_dict[py_obj]["# Imported modules"].append(row)
                     break
                 elif i == len(module_name_path.parts):
-                    py_file_import_classes_dict.setdefault(py_obj.filename, [])
-                    py_file_import_classes_dict[py_obj.filename].append(import_.abs_path)
+                    py_file_import_classes_dict.setdefault(py_obj, [])
+                    py_file_import_classes_dict[py_obj].append(import_.abs_path)
 
-        if modules:
-            files_text_dict[py_obj.filename].append(f'# Imported modules')
-            files_text_dict[py_obj.filename] += list(set(modules))
-
-    for py_file_name, classes in py_file_import_classes_dict.items():
-        user_classes = []
-        unknown_imports = []
+    for py_obj, classes in py_file_import_classes_dict.items():
         for class_ in classes:
             if class_ in all_classes_links:
-                user_classes.append(all_classes_links[class_])
+                module_name = Path("/".join(class_.split(".")[:-1])).as_posix() + ".py"
+                class_name = class_.split(".")[-1]
+                files_text_dict[py_obj]["# Imported classes"].append(f"[[{module_name}#{class_name}|{class_name}]]")
             else:
-                unknown_imports.append(class_)
+                files_text_dict[py_obj]["# Other imports"].append(class_)
 
-        if user_classes:
-            files_text_dict[py_file_name].append("# Imported classes")
-            for class_ in user_classes:
-                files_text_dict[py_file_name].append(f"[[{class_}|{class_.split('#')[1]}]]")
-        if unknown_imports:
-            files_text_dict[py_file_name].append("# Other imports")
-            for import_ in unknown_imports:
-                files_text_dict[py_file_name].append(import_)
-
-    for py_file_name, rows in files_text_dict.items():
-        doc_file = docs_path / (py_file_name + ".md")
+    for py_obj, text_dict in files_text_dict.items():
+        doc_file = docs_path / (py_obj.filename + ".md")
         doc_file.parent.mkdir(parents=True, exist_ok=True)
         try:
             with doc_file.open(mode="w", encoding='utf-8') as doc_file_obj:
+                rows = [text_dict['Header'],
+                        text_dict['link_to_file'], ]
+                for key, value in text_dict.items():
+                    if not isinstance(value, list):
+                        continue
+                    if value:
+                        rows.append(key)
+                        rows += value
                 doc_file_obj.write("\n".join(rows))
-        except:
-            pass
+        except Exception as exc:
+            raise exc
 
 
 if __name__ == '__main__':
